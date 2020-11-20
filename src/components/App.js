0b26eec7aeca00d3fc3c207927b86a7c1ff9aef8
Brian Harvey <brian.harv3y@gmail.com>
2020-11-20 14:43:46 -0800

    add background image and research styling rules

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import '../App.css';
 import Header from "./Header";
 import Body from "./Body";
+// import taproom_wallpaper from './../images/taproom_wallpaper.jpg';
+
 
 
 function App() {
+
   return(
     <React.Fragment>
       <div className="container-fluid">
         <Header/>
+        {/* <img src={taproom_wallpaper} alt="background"></img> */}
         <Body/>
       </div>
     </React.Fragment>
