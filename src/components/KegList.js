0b0864bd9190073d7a8e5e1abc20fac550794464
Brian Harvey <brian.harv3y@gmail.com>
2020-11-20 09:40:17 -0800

    add static masterKegList

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,6 +1,37 @@
 import React from 'react';
 import Keg from './Keg';
 
+const masterKegList = [
+  {
+    name: 'Gridiron Gang',
+    brand: 'Rogue',
+    price: 6,
+    alcCon: 6.5,
+    flavor: 'IPA'
+  },
+  {
+    name: 'Super Juicy',
+    brand: 'Cascade Brewing',
+    price: 6.50,
+    alcCon: 5.5,
+    flavor: 'Hazy IPA'
+  },
+  {
+    name: 'Hurly\'s Burly',
+    brand: 'Cascade Brewing',
+    price: 7,
+    alcCon: 5,
+    flavor: 'Stout'
+  },
+  {
+    name: 'Old Speckled Hen',
+    brand: 'Rare Import',
+    price: 5,
+    alcCon: 4.5,
+    flavor: 'Cream Ale'
+  }
+]
+
 function KegList(){
   return (
     <React.Fragment>
