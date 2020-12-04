8ba966f56407d12bd1d4253cc09762da29e53c20
Brian Harvey <brian.harv3y@gmail.com>
2020-12-04 09:53:39 -0800

    write and pass kegListReducer test 3

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -13,6 +13,27 @@ describe('kegListReducer', () => {
     id: 1
   }
 
+  const currentState = {
+    1: {
+      name: 'The Beast', 
+    brand: 'Doghaus',
+    flavor: 'IPA',
+    price: 7,
+    quantity: 120,
+    alcCon: 7.5,
+    id: 1
+    },
+    2: {
+    name: 'Old Sturgeon', 
+    brand: 'Tailwind',
+    flavor: 'Stout',
+    price: 8,
+    quantity: 123,
+    alcCon: 6,
+    id: 2
+    }
+  }
+
   test('Should return default state if no action is specified', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   })
@@ -42,6 +63,44 @@ describe('kegListReducer', () => {
         quantity: quantity,
         alcCon: alcCon,
         id: id
+      }
+    })
+  })
+
+  test('Should reduce quantity of keg pints by one', () => {
+    const { name, brand, flavor, quantity, price, alcCon, id } = kegProps;
+
+    action = {
+      type: 'SELL_PINT',
+      name: name,
+      brand: brand,
+      flavor: flavor,
+      price: price,
+      quantity: quantity,
+      alcCon: alcCon,
+      id: 1
+
+    }
+
+    expect(kegListReducer(currentState, action)).toEqual({
+
+      1: {
+        name: 'The Beast', 
+      brand: 'Doghaus',
+      flavor: 'IPA',
+      price: 7,
+      quantity: 119,
+      alcCon: 7.5,
+      id: 1
+      },
+      2: {
+      name: 'Old Sturgeon', 
+      brand: 'Tailwind',
+      flavor: 'Stout',
+      price: 8,
+      quantity: 123,
+      alcCon: 6,
+      id: 2
       }
     })
   })
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -13,6 +13,22 @@ export default (state = {}, action) => {
          id: id
        }
      });
+
+
+   case 'SELL_PINT':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        flavor: flavor,
+        alcCon: alcCon,
+        quantity: quantity - 1,
+        price: price,
+        id: id
+      }
+    });  
+
+
    default:
      return state;  
  }
